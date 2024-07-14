5fb34440d3eda39a02672740561bd0dc0b4cc6c0
Jai <jsarava2@ncsu.edu>
2024-07-14 17:27:44 -0400

    Navbar Test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Yo</h1>
+    <div>
+      <Navbar />
     </div>
   );
 }
