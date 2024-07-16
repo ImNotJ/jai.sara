782be25662e4bdabea7b38bf6cb6b1ac252c7fa7
Jai <jsarava2@ncsu.edu>
2024-07-15 22:32:15 -0400

    Complete navbar styling and add github and linkedin links

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,30 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // import { BiSearch } from 'react-icons/bi';
 // import { BsPerson } from 'react-icons/bs';
-// import { HiOutlineMenuAlt4 } from 'react-icons/hi';
-// import { AiOutlineClose } from 'react-icons/ai';
-import { FaFacebook, FaInstagram, FaPinterest, FaTwitter, FaYoutube } from 'react-icons/fa';
+import { LiaGithubSquare, LiaLinkedin } from 'react-icons/lia';
 
 import './NavbarStyles.css';
 
 function Navbar() {
     const [nav, setNav] = useState(false);
     const [active, setActive] = useState(false);
+    const [time, setTime] = useState(new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
 
     const handleNav = () => {
         setNav(!nav);
         setActive(!active);
     };
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <div className="navbar">
             <div className="start-button">
-                <button 
-                    className={active ? 'active' : ''} 
+                <button
+                    className={active ? 'active' : ''}
                     onClick={handleNav}>
                     Start
                 </button>
             </div>
+            <div className="divider"></div>
+            <div className="social-icons">
+                <a href="https://github.com/ImNotJ" target="_blank" rel="noopener noreferrer">
+                    <LiaGithubSquare className='icon' style={{color: 'var(--pink)'}}/>
+                </a>
+                <a href="https://www.linkedin.com/in/jaisaravanan/" target="_blank" rel="noopener noreferrer">
+                    <LiaLinkedin className='icon' style={{color: 'var(--blue)'}}/>
+                </a>
+            </div>
+            <div className="divider"></div>
             <div className={nav ? 'mobile-menu active' : 'mobile-menu'}>
                 <ul className="mobile-nav">
                     <li>Home</li>
@@ -38,14 +54,10 @@ function Navbar() {
                         <button>Search</button>
                         <button>Account</button>
                     </div>
-                    <div className="social-icons">
-                        <FaFacebook className='icon' />
-                        <FaInstagram className='icon' />
-                        <FaTwitter className='icon' />
-                        <FaPinterest className='icon' />
-                        <FaYoutube className='icon' />
-                    </div>
                 </div>
+            </div>
+            <div className="clock">
+                {time}
             </div>
         </div>
     );
